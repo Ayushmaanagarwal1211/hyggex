61fe986beb9293a09a611ce7d9b74695026dadd7
Ayushmaanagarwal1211 <118350936+Ayushmaanagarwal1211@users.noreply.github.com>
2024-01-16 23:30:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigationbar from './components/Navigationbar';
+import Flashcard from './components/Flashcard';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+    <Navigationbar/>
+    <Flashcard />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+ <>   <div className='absolute flash mt-[1190px] ml-[104px]' style={{fontWeight:"700",fontSize:"48px"}}>FAQ</div>
+ 
+ <div className='flex absolute  mt-[1273px] ml-[104px] flex-col gap-[32px]'>
+<div className='pt-[19px] w-[848px] pb-[12px] h-[58px] pl-[23px] bordercolor grid' style={{border:"solid blue 1px",borderRadius:"12px"}}><p className='relative bottom-1' style={{fontWeight:"600",color:"rgba(40, 38, 44, 1)"}}>Can education flashcards be used for all age groups?</p><p className='justify-self-end relative right-2 bottom-[30px]'><select className='bg-white'></select></p></div>
+<div className='pt-[19px] w-[848px] pb-[12px] h-[58px] pl-[23px] grid bordercolor' style={{border:"solid blue 1px",borderRadius:"12px"}}><p className='relative bottom-1' style={{fontWeight:"600",color:"rgba(40, 38, 44, 1)"}}>Can education flashcards be used for all age groups?</p><p className='justify-self-end relative right-2 bottom-[30px]'><select className='bg-white'></select></p></div>
+<div className='pt-[19px] w-[848px] pb-[12px] h-[58px] pl-[23px] grid bordercolor' style={{border:"solid blue 1px",borderRadius:"12px"}}><p className='relative bottom-1' style={{fontWeight:"600",color:"rgba(40, 38, 44, 1)"}}>Can education flashcards be used for all age groups?</p><p className='justify-self-end relative right-2 bottom-[30px]'><select className='bg-white'></select></p></div>
+
+ </div>
+ </>
+  )
+}
